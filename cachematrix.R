833a2d2fb1f918b838c70d01949f723f2dd37e63
mustakimahmad <34410044+mustakimahmad@users.noreply.github.com>
2017-12-24 12:30:04 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,27 @@
 
 ## Write a short comment describing this function
 
+
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    # Define function to set the value of the matrix
+    set <- function(y) {
+        x <<- y    # Set value
+        m <<- NULL # Clear  cache
+    }
+        
+    # Define function value of the matrix
+    get <- function() x
+    # Define function to set the inverse
+    setInverse <- function(inverse) m <<- inverse
+    # Define function to get the inverse
+    getInverse <- function() m
+    
+    # Return a list with the above four functions
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
 
